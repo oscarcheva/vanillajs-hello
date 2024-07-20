6dc51ece6b2c3379a0fd4a459d33b2d2819ba057
oscarcheva <57965843+oscarcheva@users.noreply.github.com>
2024-07-20 02:38:58 +0000

    Excuse generator completed

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["farted", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car", "my mom"];
+  let when = [
+    "before the walk in the park",
+    "when I was on the toilet",
+    "while I was exercising",
+    "during my meditation"
+  ];
+
+  var excuse =
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    action[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 4)] +
+    " " +
+    when[Math.floor(Math.random() * 4)];
+
+  document.getElementById("excuse").textContent = excuse;
 };
